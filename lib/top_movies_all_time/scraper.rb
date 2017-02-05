0a18a8c907c3c34f44f66fe05a049d2cdb82c201
Kevin Adkins <kevinladkins@gmail.com>
2017-02-05 23:33:19 +0000

    built first scraper methods

--- lib/top_movies_all_time/scraper.rb
+++ lib/top_movies_all_time/scraper.rb
@@ -1,3 +1,15 @@
 class TopMoviesAllTime::Scraper
 
+  def get_domestic_list
+    Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/domestic.htm"))
+  end
+
+  def get_worldwide_list
+    Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/world/"))
+  end
+
+  def get_adjusted_list
+    Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/adjusted.htm"))
+  end
+
 end
