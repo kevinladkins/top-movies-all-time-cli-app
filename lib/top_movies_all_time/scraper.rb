191428d3864ffa8872f4332288cf0d75ee77fbdf
Kevin Adkins <kevinladkins@gmail.com>
2017-02-06 00:25:12 +0000

    refactored/reverted to previous method structure

--- lib/top_movies_all_time/scraper.rb
+++ lib/top_movies_all_time/scraper.rb
@@ -9,33 +9,24 @@ class TopMoviesAllTime::Scraper
   end
 
   def get_adjusted_list
-    html = Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/adjusted.htm"))
-    list = html.css("div#main div#body table table tr td[2]")
+    Nokogiri::HTML(open("http://www.boxofficemojo.com/alltime/adjusted.htm"))
+  end
+
+
+  def scrape_list(list)
+    list.css("div#main div#body table table tr td[2]")
+  end
+
+  def make_movies
     movies = []
-    list.each do |t|
+    self.scrape_list(self.get_adjusted_list).each do |t|
       movies << {
-        :title => t.css("a b").text,
-        :url => "http://www.boxofficemojo.com#{t.css("a").attribute("href").value}"
+       :title => t.css("a b").text,
+       :url => "http://www.boxofficemojo.com#{t.css("a").attribute("href").value}"
       }
     end
     movies
   end
-
-
-  def scrape_list
-
-  end
-
-#  def make_movies
-#    movies = []
-#    self.get_adjusted_list.each do |t|
-#      movies << {
-#        :title => t.css("a b").text,
-#        :url => "http://www.boxofficemojo.com#{t.css("a").attribute("href").value}"
-#      }
-#    end
-#    movies
-#  end
 
 
 end
