b24d4975914bf7cc58b161e4e70f8d84bf012641
Kevin Adkins <kevinladkins@gmail.com>
2017-02-06 00:43:32 +0000

    added initial attr_accessors and @@all array

--- lib/top_movies_all_time/movie.rb
+++ lib/top_movies_all_time/movie.rb
@@ -1,3 +1,6 @@
 class TopMoviesAllTime::Movie
+  attr_accessor :title, :url, :rank_adjusted, :rank_domestic, :rank_worldwide, :domestic_gross, :adjusted_gross, :worldwide_gross, :tickets_sold, :release_date
+
+  @@all = []
 
 end
