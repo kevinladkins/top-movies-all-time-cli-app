74f1e6238fec6b34e88933a614ff9046ff260800
Kevin Adkins <kevinladkins@gmail.com>
2017-02-06 20:50:47 +0000

    still building start, built #print_domestic_list

--- lib/top_movies_all_time/cli.rb
+++ lib/top_movies_all_time/cli.rb
@@ -11,8 +11,23 @@ class TopMoviesAllTime::CLI
     puts "1. US Domestic Box Office"
     puts "2. US Domestic Box Office - Adjusted for Inflation"
     puts "3. Worldwide Box Office"
+    input = gets.chomp
+    while input != "exit"
+      if input == "1" || input == "US Domestic Box Office"
+        print_domestic_list
+      elsif input == "2" || input == "US Domestic Box Office - Adjusted for Inflation"
+        print_adjusted_list
+      elsif input == "3" || input == "Worldwide Box Office"
+        print_worldwide_list
+      else
+        start
+      end
+    end
+    puts "Thanks for stopping by!"
+  end
 
-
+  def print_domestic_list
+    puts TopMoviesAllTime::Scraper.domestic_rankings
   end
 
 end
